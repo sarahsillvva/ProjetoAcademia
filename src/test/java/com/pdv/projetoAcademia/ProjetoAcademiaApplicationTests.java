5704e03accac16caca6be0b4ad61dfedf22720fc
sarah silva <sarah.silvaon@gmail.com>
2022-03-25 18:11:05 -0300

    envio de projeto

---
+++ src/test/java/com/pdv/projetoAcademia/ProjetoAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdv.projetoAcademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
